2106ff4921edfb72abadb3889f080ed9d1bedb12
jonas-martinez <cbobbxta.fr@gmail.com>
2022-06-14 12:04:34 +0200

    test: Add views tests

---
+++ test/views/activation_code_page_test.dart
@@ -0,0 +1,31 @@
+import 'package:client_backoffice/views/activation_code_page.dart';
+import 'package:client_common/models/auth_model.dart';
+import 'package:client_common/test/lenra_page_test_help.dart';
+import 'package:client_common/views/simple_page.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  testWidgets('ActivationCodePage check SimplePage', (WidgetTester tester) async {
+    await tester.pumpWidget(createAppTestWidgets(ChangeNotifierProvider<AuthModel>(
+      create: (_) => AuthModel(),
+      child: ActivationCodePage(),
+    )));
+
+    final widgetFinder = find.byType(SimplePage);
+
+    expect(widgetFinder, findsOneWidget);
+  });
+
+  testWidgets('ActivationCodePage check texts', (WidgetTester tester) async {
+    await tester.pumpWidget(createAppTestWidgets(ChangeNotifierProvider<AuthModel>(
+      create: (_) => AuthModel(),
+      child: ActivationCodePage(),
+    )));
+
+    final textFinder = find.byType(Text);
+
+    expect(textFinder, findsNWidgets(5));
+  });
+}
