f0d21e966dd80f6bca6f76980de61c039ca6d129
Jonas Martinez <36544012+jonas-martinez@users.noreply.github.com>
2022-07-19 09:43:20 +0200

    feat: Add Sentry (#19)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:client_backoffice/navigation/backoffice_navigator.dart';
+import 'package:client_common/config/config.dart';
 import 'package:client_common/models/auth_model.dart';
 import 'package:client_common/models/build_model.dart';
 import 'package:client_common/models/cgu_model.dart';
@@ -6,10 +7,31 @@ import 'package:client_common/models/store_model.dart';
 import 'package:client_common/models/user_application_model.dart';
 import 'package:flutter/material.dart';
 import 'package:lenra_components/lenra_components.dart';
+import 'package:logging/logging.dart';
 import 'package:provider/provider.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
 void main() async {
-  runApp(Backoffice());
+  Logger.root.level = Level.WARNING;
+  Logger.root.onRecord.listen((record) {
+    print('${record.level.name}: ${record.time}: ${record.message}');
+  });
+
+  debugPrint("Starting main app[debugPrint]: ${Config.instance.application}");
+  // TODO: Récupération de variables d'environnement ne doit pas marcher
+  const environment = String.fromEnvironment('ENVIRONMENT');
+
+  if (environment == "production" || environment == "staging") {
+    const sentryDsn = String.fromEnvironment('SENTRY_CLIENT_DSN');
+    await SentryFlutter.init(
+      (options) => options
+        ..dsn = sentryDsn
+        ..environment = environment,
+      appRunner: () => runApp(Backoffice()),
+    );
+  } else {
+    runApp(Backoffice());
+  }
 }
 
 class Backoffice extends StatelessWidget {
