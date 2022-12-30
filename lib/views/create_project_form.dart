bd652d02b283bc739af0ddb28022c60670131a61
jonas-martinez <cbobbxta.fr@gmail.com>
2022-12-30 16:41:36 +0100

    feat: Cancel app creation button

--- lib/views/create_project_form.dart
+++ lib/views/create_project_form.dart
@@ -1,4 +1,5 @@
 import 'package:client_backoffice/navigation/backoffice_navigator.dart';
+import 'package:client_common/models/auth_model.dart';
 import 'package:client_common/models/user_application_model.dart';
 import 'package:client_common/navigator/common_navigator.dart';
 import 'package:client_common/views/loading_button.dart';
@@ -25,6 +26,7 @@ class _CreateProjectFormState extends State<CreateProjectForm> {
   @override
   Widget build(BuildContext context) {
     bool isLoading = context.select<UserApplicationModel, bool>((m) => m.createApplicationStatus.isFetching());
+    bool userHasApp = context.read<UserApplicationModel>().userApps.isNotEmpty;
     return Form(
       key: _formKey,
       child: LenraFlex(
@@ -33,12 +35,32 @@ class _CreateProjectFormState extends State<CreateProjectForm> {
         crossAxisAlignment: CrossAxisAlignment.end,
         children: [
           fields(context),
-          LoadingButton(
-            text: "Create my project",
-            loading: isLoading,
-            onPressed: () {
-              submit();
-            },
+          LenraFlex(
+            spacing: 8,
+            children: [
+              LenraButton(
+                text: userHasApp ? "Cancel" : "Logout",
+                onPressed: () {
+                  if (userHasApp) {
+                    CommonNavigator.go(context, BackofficeNavigator.selectProject);
+                  } else {
+                    context.read<AuthModel>().logout().then((value) {
+                      CommonNavigator.go(context, CommonNavigator.login);
+                    }).catchError((error) {
+                      logger.warning(error);
+                    });
+                  }
+                },
+                type: LenraComponentType.tertiary,
+              ),
+              LoadingButton(
+                text: "Create my project",
+                loading: isLoading,
+                onPressed: () {
+                  submit();
+                },
+              ),
+            ],
           ),
         ],
       ),
