dc5fb8a67d9243882323410b845767250060a3cb
jonas.martinez <cbobbxta.fr@gmail.com>
2023-08-22 14:56:21 +0200

    feat: dynamic OAuth client id

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,6 @@
 
 sed -i "s|\${LENRA_SERVER_URL}|${LENRA_SERVER_URL}|g" index.html
 sed -i "s|\${SENTRY_CLIENT_DSN}|${SENTRY_CLIENT_DSN}|g" index.html
+sed -i "s|\${OAUTH_CLIENT_ID}|${OAUTH_CLIENT_ID}|g" index.html
 
 /bin/bash -c "/opt/bitnami/scripts/nginx/entrypoint.sh $@"
