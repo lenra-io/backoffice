775226764c80f476bb9ca8326d9d51466b75fab2
jonas.martinez <cbobbxta.fr@gmail.com>
2023-08-28 21:16:14 +0200

    feat: OAuth Page Design

--- lib/navigation/guard.dart
+++ lib/navigation/guard.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:client_backoffice/navigation/backoffice_navigator.dart';
+import 'package:client_backoffice/views/oauth_page.dart';
 import 'package:client_common/navigator/guard.dart';
 import 'package:flutter/material.dart';
 
@@ -11,7 +12,17 @@ class BackofficeGuard extends Guard {
 
   static final BackofficeGuard checkHaveApp = BackofficeGuard(isValid: Guard.haveApp(true), onInvalid: _toWelcome);
 
+  static final Guard checkIsAuthenticated = Guard(isValid: isAuthenticated, onInvalid: toOauth);
+
+  static Future<bool> isAuthenticated(BuildContext context) async {
+    return OAuthPageState.isAuthenticated(context);
+  }
+
   static String _toWelcome(context) {
     return BackofficeNavigator.welcome.path;
+  }
+
+  static String toOauth(context) {
+    return BackofficeNavigator.oauth.path;
   }
 }
