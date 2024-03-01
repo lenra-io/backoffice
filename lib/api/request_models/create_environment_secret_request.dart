f20b731a8012958458e1120e951ace5fba08efb0
jonas.martinez <cbobbxta.fr@gmail.com>
2024-03-01 16:22:37 +0100

    fix

--- lib/api/request_models/create_environment_secret_request.dart
+++ lib/api/request_models/create_environment_secret_request.dart
@@ -3,17 +3,14 @@ import 'package:client_common/api/request_models/api_request.dart';
 class CreateEnvironmentSecretRequest extends ApiRequest {
   String key;
   String value;
-  bool isObfuscated;
 
   CreateEnvironmentSecretRequest({
     required this.key,
     required this.value,
-    this.isObfuscated = true,
   });
 
   Map<String, dynamic> toJson() => {
         "key": key,
         "value": value,
-        "is_obfuscated": isObfuscated,
       };
 }
